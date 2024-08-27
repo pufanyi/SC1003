93f814a5dcd31128fd66519ef7a929f1f4d7180a
Pu Fanyi <pufanyi@gmail.com>
2024-08-28 02:12:39 +0800

    chore: Add lab 2 template and assign files

---
+++ README.md
@@ -0,0 +1,9 @@
+# SC1003-Answers
+
+## Lab 1
+
+- [slide](https://docs.google.com/presentation/d/1k5z4LN1t6XNF-gg_Qn1gc4UgJkRsxBXu6CEy1WkjJG4/edit?usp=sharing)
+
+## Lab 2
+
+- [slide]()
---
+++ lab2/template.py
@@ -0,0 +1,35 @@
+# TODO : 1. There are two players in the game. Initialize their names as string
+# Add you code of TODO 1 here
+
+# TODO : 2. There are two layers in the sea. Initialize the integer variable layer
+# Add you code of TODO 2 here
+
+# TODO : 3. The battleship board is 10 by 10 dimension. Initialize the integer variables width and height
+# Add you code of TODO 3 here
+
+# TODO : 4. Initialize a boolean variable that will be used to indicate whether user input is valid or not, two boolean variables hit and miss that indicate whether the ship is hit or missed
+# Add you code of TODO 4 here
+
+# TODO : 5. The ships have only two orientation, either vertical or horizontal. Initialize a variable as ship orientation
+# Add you code of TODO 5 here
+
+# TODO : 6. The coordinates of where to put ship or where to hit on the board have a specific format. Create a string variable as coordinates
+# Add you code of TODO 6 here
+
+# TODO : 7. There are only two types of ships. Initialize two string variables as ship names
+# Add you code of TODO 7 here
+
+# TODO : 8. Initialize a string variable holding a welcome message that can be displayed to user
+# Add you code of TODO 8 here
+
+print(player1, player2)
+print(layer)
+print(width, height)
+print(valid, hit, miss)
+print(ori)
+print(coor)
+print(ship1, ship2)
+print(welmes)
+
+# TODO : 9. Take user input for attack coordinates and display the result.
+# Add you code of TODO 9 here
