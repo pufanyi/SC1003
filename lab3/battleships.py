fbb4d528104ce36b860f932448829eb9705810b4
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-09-04 01:42:16 +0800

    chore: Validate user input for coordinates and orientation in battleships game

--- lab3/battleships.py
+++ lab3/battleships.py
@@ -37,10 +37,22 @@ x, y, orientation = 4, 6, 'horizontal'
 # TODO : 3. Validate user input for coordinates and orientation
 # Add you code of TODO 3 here
 
+# valid = 0 <= x < board_size and 0 <= y < board_size and orientation in valid_orientations
+
+if 0 <= x < board_size and 0 <= y < board_size and orientation in valid_orientations:
+    valid = True
+else:
+    valid = False
+print(f"Input ({x}, {y}, {orientation}) is valid: {valid}")
 
 # One more example
 x, y, orientation = 11, 3, 'diagonal'
 
+if 0 <= x < board_size and 0 <= y < board_size and orientation in valid_orientations:
+    valid = True
+else:
+    valid = False
+print(f"Input ({x}, {y}, {orientation}) is valid: {valid}")
 
 # TODO : 4. Validate user input for coordinates and orientation
 # Copy your code of TODO 3 here to test on x, y, orientation = 11, 3, 'diagonal'
@@ -53,8 +65,39 @@ x, y, ship_length, orientation = 3, 5, 4, 'horizontal'
 # TODO : 5. Validate the placement of a ship
 # Add you code of TODO 5 here
 # One more example
+
+valid = True
+if orientation == 'horizontal':
+    x_end = x + ship_length - 1
+    y_end = y
+elif orientation == 'vertical':
+    x_end = x
+    y_end = y + ship_length - 1
+else:
+    valid = False
+
+if not (0 <= x < board_size and 0 <= y < board_size and 0 <= x_end < board_size and 0 <= y_end < board_size):
+    valid = False
+    
+print(f"Placement ({x}, {y}, {ship_length}, {orientation}) is valid: {valid}")
+
 x, y, ship_length, orientation = 7, 7, 4, 'vertical'
 # TODO : 6. Validate the placement of a ship
 # Copy your code of TODO 5 here to test on x, y, ship_length, orientation = 7, 7, 4, 'vertical'
+
+valid = True
+if orientation == 'horizontal':
+    x_end = x + ship_length - 1
+    y_end = y
+elif orientation == 'vertical':
+    x_end = x
+    y_end = y + ship_length - 1
+else:
+    valid = False
+
+if not (0 <= x < board_size and 0 <= y < board_size and 0 <= x_end < board_size and 0 <= y_end < board_size):
+    valid = False
+    
+print(f"Placement ({x}, {y}, {ship_length}, {orientation}) is valid: {valid}")
 
 ###################################################################
